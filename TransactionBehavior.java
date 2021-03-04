4be82d7e72c2de5ecb7293957fa1e952822d8c45
Kiran B S <kbs71190@gmail.com>
2021-03-04 16:51:18 +0530

    Create TransactionBehavior.java

---
+++ TransactionBehavior.java
@@ -0,0 +1,45 @@
+public class TransactionBehaviour {
+   // set is either using xml Or annotation
+    DataSourceTransactionManager manager=new DataSourceTransactionManager();
+    SimpleTransactionStatus status=new SimpleTransactionStatus();
+   ;
+  
+    
+    public void beginTransaction()
+    {
+        DefaultTransactionDefinition Def = new DefaultTransactionDefinition();
+        // overwrite default PROPAGATION_REQUIRED and ISOLATION_DEFAULT
+        // set is either using xml Or annotation
+        manager.setPropagationBehavior(XX);
+        manager.setIsolationLevelName(XX);
+       
+        status = manager.getTransaction(Def);
+    
+    }
+
+    public void commitTransaction()
+    {
+       
+      
+            if(status.isCompleted()){
+                manager.commit(status);
+        } 
+    }
+
+    public void rollbackTransaction()
+    {
+       
+            if(!status.isCompleted()){
+                manager.rollback(status);
+        }
+    }
+    Main method{
+        beginTransaction()
+        M1();
+        If error(){
+            rollbackTransaction()
+        }
+         commitTransaction();
+    }
+   
+}
